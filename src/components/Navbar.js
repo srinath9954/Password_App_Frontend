53015a8d3fc7112ffb4290ae7dcf66d06eecc5fb
Srinath P <116104940+srinath9954@users.noreply.github.com>
2024-08-13 01:59:50 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Container, Button } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 
-const Navbar = () => {
+const Navbar = ({ deferredPrompt, handleInstallClick }) => {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#2c3e50' }}>
       <Container maxWidth="lg">
@@ -13,6 +13,25 @@ const Navbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
             CipherSafe
           </Typography>
+          {deferredPrompt && (
+            <Button
+              onClick={handleInstallClick}
+              sx={{
+                backgroundColor: 'black',
+                color: 'white',
+                borderRadius: '40px',
+                padding: '5px 10px',
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                fontSize: '16px',
+                cursor: 'pointer',
+                '&:hover': {
+                  backgroundColor: 'purple',
+                },
+              }}
+            >
+              Install App
+            </Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
